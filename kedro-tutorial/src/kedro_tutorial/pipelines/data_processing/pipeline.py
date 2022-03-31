5fbeaf4d8cbc0c8d4465a4692a3b8cccda4b336a
maxschulz-COL <maximilian_schulz@mckinsey.com>
2022-03-31 15:42:11 +0200

    Add model input node

--- kedro-tutorial/src/kedro_tutorial/pipelines/data_processing/pipeline.py
+++ kedro-tutorial/src/kedro_tutorial/pipelines/data_processing/pipeline.py
@@ -6,6 +6,7 @@ generated using Kedro 0.17.7
 from kedro.pipeline import Pipeline, node, pipeline
 
 from .nodes import preprocess_companies, preprocess_shuttles
+from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
@@ -21,6 +22,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs="shuttles",
                 outputs="preprocessed_shuttles",
                 name="preprocess_shuttles_node",
+            ),
+            node(
+                func=create_model_input_table,
+                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
+                outputs="model_input_table",
+                name="create_model_input_table_node",
             ),
         ]
     )
